9b1f419df3e5559a162721e3f8da8d9d4b8f2f3b
Zach Robichaud <zach2825@gmail.com>
2021-05-10 11:20:40 -0400

    Generate the new branch from a specific a specific branch for AHA

--- app/Commands/AHATaskGet.php
+++ app/Commands/AHATaskGet.php
@@ -5,7 +5,6 @@ namespace App\Commands;
 use App\Contracts\GitStuff;
 use App\Models\AHATask;
 use App\Services\AHA;
-use App\Services\Azure;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 
@@ -17,6 +16,8 @@ class AHATaskGet extends Command
      * @var string
      */
     protected $signature = 'aha:task {task_id : task_id to fetch from aha boards without the key only the number like 1234}
+                                        {--f|from= : Which branch to generate from}
+                                        {--g|guess : branch from develop or master depending if the task is hotfix or not}
                                         {--d|dump : only output the task details}
                                         {--b|dumpBranchName : only output the task branch name}
     ';
@@ -26,7 +27,7 @@ class AHATaskGet extends Command
      *
      * @var string
      */
-    protected $description = 'Generate a token from aha';
+    protected $description = 'Read task details from AHA and generate the branch name from those details.';
 
     /**
      * Execute the console command.
@@ -40,6 +41,7 @@ class AHATaskGet extends Command
         /** @var AHATask $task */
         $task    = null;
         $task_id = $this->argument('task_id');
+        $guess   = $this->option('guess');
 
         $this->task(
             'Getting task from aha',
@@ -48,22 +50,44 @@ class AHATaskGet extends Command
             }
         );
 
-        if($this->option('dump')){
+        if ($this->option('dump')) {
             $this->info(print_r($task->toArray(), true));
             return 0;
         }
 
+        $from       = $this->option('from');
         $branchName = '';
-        $this->task('Creating the branch name', function() use ($gitStuff, $task, $aha, &$branchName) {
-            $branchName = $gitStuff->branchNameTemplate($task->title, $task->id, $aha->mapStatusToBranchType($task->workType), config('aha.task_key'));
-        });
+        $this->task(
+            'Creating the branch name',
+            function () use ($gitStuff, $task, $aha, &$branchName, &$from, $guess) {
+                $branchName = $gitStuff->branchNameTemplate(
+                    $task->title,
+                    $task->id,
+                    $branchType = $aha->mapStatusToBranchType($task->workType),
+                    config('aha.task_key')
+                );
 
-        if($this->option('dumpBranchName')){
+                if ($guess) {
+                    if ($branchType == 'hotfix') {
+                        $from = 'main';
+                    } else {
+                        $from = 'develop';
+                    }
+                }
+            }
+        );
+
+        if ($this->option('dumpBranchName')) {
             $this->info('');
             $this->info('The branch name would be "' . $branchName . '"');
             $this->info("git checkout '$branchName' 2> /dev/null || git checkout -b '$branchName");
             $this->info('');
             return 0;
+        }
+
+        if ($from) {
+            $this->info(sprintf('Generating the new branch "%s" from "%s"', $branchName, $from));
+            $gitStuff->checkout($from);
         }
 
         $gitStuff->checkout($branchName);
@@ -74,7 +98,7 @@ class AHATaskGet extends Command
     /**
      * Define the command's schedule.
      *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param Schedule $schedule
      * @return void
      */
     public function schedule(Schedule $schedule): void
