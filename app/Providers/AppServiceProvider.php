2d05a3daa92c1ee91c48d0ed885f32c1989223d0
The Zach <zach2825@gmail.com>
2022-12-06 14:36:23 -0500

    Find the service name better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,8 @@ namespace App\Providers;
 use App\Contracts\GitStuff;
 use App\Contracts\PullTaskNumber;
 use App\Contracts\TaskService;
-use App\enums\ImportTypesEnums;
-use App\Services\AHA;
-use App\Services\Azure;
-use App\Services\JIRA;
 use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -39,14 +36,13 @@ class AppServiceProvider extends ServiceProvider
                 /**
                  * react the repo settings and decide which task service to use
                  */
-                $task_service_id = GitStuff::getOrgService();
+                $task_service_name = GitStuff::getOrgService();
 
-                return match ($task_service_id) {
-                    ImportTypesEnums::AHA->name => $this->app->make(AHA::class),
-                    ImportTypesEnums::AZURE->name => $this->app->make(Azure::class),
-                    ImportTypesEnums::JIRA->name => $this->app->make(JIRA::class),
-                    default => throw new Exception('Unexpected value'),
-                };
+                try {
+                    return $this->app->make("App\\Services\\{$task_service_name}");
+                } catch (BindingResolutionException $exception) {
+                    throw new Exception("Unexpected value: {$task_service_name}");
+                }
             }
         );
     }
